b69863443fc7a9eb477a30d5a5cf132975d83fc5
Lars Asplund <lars.asplund@hotmail.com>
2023-01-14 08:50:17 +0100

    Add project files.

---
+++ Kund_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kund_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // En konstruktor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        // kommer skapa en table i sql databasen
+        public DbSet<Companies> Company { get; set; }
+    }
+}
